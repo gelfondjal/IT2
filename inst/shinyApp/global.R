f85a66ff520f46f676ea501601a48bca39ee4215
Jonathan Gelfond <gelfondjal@uthscsa.edu>
2014-09-08 10:30:08 -0500

    fix library(pander) app error

--- inst/shinyApp/global.R
+++ inst/shinyApp/global.R
@@ -2,6 +2,7 @@
 if(!require(shinyIncubator)){devtools::install_github("shiny-incubator", "rstudio")}
 require(shinyIncubator)
 library(devtools)
+library(pander)
 library(IT2)
 panderOptions("table.split.table",Inf)
 all.orchards <-get_orchard()
