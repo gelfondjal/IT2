ae80311ebc6d2d791904c485ce77dd238604d896
Jonathan Gelfond <gelfondjal@uthscsa.edu>
2014-12-18 15:08:32 -0600

    use project path aware harvest

--- R/Harvest_trees.R
+++ R/Harvest_trees.R
@@ -9,9 +9,47 @@ Harvest.trees <- function(dependency.dir){
   
   dep.files <- list.files(dependency.dir,full.names=TRUE)
   
+  trees <- NULL
+  
+  if(length(dep.files)>0){
+    
   list.deps <- lapply(dep.files,read.dependency)
   
   trees <- rbind.fill(list.deps)
+    
+  project.id <-  list.deps[[1]]$project.id[1] 
+    
+  old.project.path <- unique(trees$project.path)[1]
+  
+  
+  new.path <- get.project.path(project.id)
+  
+  if((old.project.path!=new.path)|(length(old.project.path>1))){
+      
+    shaved.variables <- c("path","source.file.path","target.path","project.path")
+  
+    for(char.shave in shaved.variables){
+      
+      for(file.iter in 1:nrow(trees)){
+        
+        old.project.path <- trees$project.path[file.iter]   
+        
+        trees[[char.shave]][file.iter] <- gsub(old.project.path,"",trees[[char.shave]][file.iter],fixed=TRUE)
+        
+        trees[[char.shave]][file.iter] <- paste0(new.path,trees[[char.shave]][file.iter])
+        
+      }
+      
+    }
+    
+    
+    
+    
+  } # if project path mismatch
+  
+  
+  } # if there exist dependency files
+  
   
   return(trees)
 }
