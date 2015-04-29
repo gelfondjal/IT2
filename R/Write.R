6b4c26072f6f4831ce042cfa24f7320367b7b5f3
Jonathan Gelfond <gelfondjal@uthscsa.edu>
2015-04-29 16:00:21 -0500

    add date to Write

--- R/Write.R
+++ R/Write.R
@@ -2,14 +2,29 @@
 #' @param file.name file to write to the source "Result" directory
 #' @param description describes object to write
 #' @param write.fcn function for writing file of object type
+#' @param date logical for adding date to filename
 #' @return File information list 
 #' @export
 
 
-Write <- function(obj=NULL,file.name="data.csv",description="Result file",write.fcn=guess.write.fcn(file.name),...){
+Write <- function(obj=NULL,file.name="data.csv",description="Result file",write.fcn=guess.write.fcn(file.name),date=FALSE,...){
   
   # lightweight Write.cap take small number of args
   # used file.name and description create file.information
+  
+  if(date){
+    
+    prefix <- gsub("\\..*","",file.name)
+    suffix <- gsub(".*\\.","\\.",file.name)
+    
+    datestring <- format(Sys.time(), "%d %b %Y")
+    
+    
+    filenameout <- paste0(prefix,"_",datestring,suffix)
+    
+    file.name <- gsub(" ","_",file.name)
+    
+  }
   
   
   if(dirname(file.name)!="."){
